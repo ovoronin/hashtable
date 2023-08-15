a85c52390949b889bd0a1b14574fd8f7bf0d1700
Oleg Voronin <ovoronin@corevalue.net>
2023-08-15 15:31:19 +0300

    1 iteration

--- src/hashtable.spec.ts
+++ src/hashtable.spec.ts
@@ -1,13 +1,34 @@
 import { HashTable } from "./hashtable"
 
 describe("hash table", () => {
+  let hashtable: HashTable;
+
+  beforeEach(() => {
+    hashtable = new HashTable();
+  });
+
   it("should exist", () => {
     expect(HashTable).toBeTruthy();
   });
 
   it("should have a constructor", () => {
-    const hashtable = new HashTable();
     expect(hashtable instanceof HashTable).toBe(true);
+  });
+
+  it("should set a value", () => {
+    expect(() => hashtable.set('a', 1)).not.toThrow();
+  });
+
+  it("should get undefined if a value is not found", () => {
+    expect(hashtable.get('a')).toBe(undefined);
+  });
+
+  it("should get a value", () => {
+    hashtable.set('a', 1);
+    hashtable.set('b', 2);
+
+    expect(hashtable.get('a')).toBe(1);
+    expect(hashtable.get('b')).toBe(2);
   });
 
 });
--- src/hashtable.ts
+++ src/hashtable.ts
@@ -1,3 +1,27 @@
-export class HashTable {
+export class HashTable<T = any> {
+  private max = 8;
+  private data: Array<T> = [];
 
+  private hashFn(key: string, max: number): number {
+    const sum = key.split('').reduce<number>((s, char) => s += char.charCodeAt(0), 0);
+    return sum % max;
+  }
+
+  private getIndex(key: string): number {
+    return this.hashFn(key, this.max);
+  }
+
+  constructor() {
+    this.data = new Array(this.max).fill(undefined);
+  }
+
+  set(key: string, value: T) {
+    const index = this.getIndex(key);
+    this.data[index] = value;
+  }
+
+  get(key: string): T | undefined {
+    const index = this.getIndex(key);
+    return this.data[index];
+  }
 }
