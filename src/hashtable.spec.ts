b51ac17b6c749d4d17dbc0cf61b74073322f3cf8
Oleg Voronin <ovoronin@corevalue.net>
2023-08-18 16:33:25 +0300

    refactor again

--- src/hashtable.spec.ts
+++ src/hashtable.spec.ts
@@ -1,7 +1,9 @@
-import { HashTable, hashFn } from "./hashtable"
+import { HashTable } from "./hashtable"
 
 describe("hash table", () => {
-  let hashtable: HashTable;
+  let h: HashTable;
+  const collidedKey1 = '19YN4';
+  const collidedKey2 = 'hgLkU';
 
   const randomKey = (length: number): string => {
     let result = '';
@@ -16,58 +18,55 @@ describe("hash table", () => {
   }
 
   beforeEach(() => {
-    hashtable = new HashTable();
+    h = new HashTable();
   });
 
   it("should exist", () => {
-    expect(HashTable).toBeTruthy();
-  });
-
-  it("should have a constructor", () => {
-    expect(hashtable instanceof HashTable).toBe(true);
+    expect(h).toBeTruthy;
   });
 
   it("should set a value", () => {
-    expect(() => hashtable.set('a', 1)).not.toThrow();
+    expect(() => h.set('a', 1)).not.toThrow();
   });
 
   it("should get undefined if a value is not found", () => {
-    expect(hashtable.get('a')).toBe(undefined);
+    expect(h.get('a')).toBe(undefined);
   });
 
   it("should get a value", () => {
-    hashtable.set('a', 1);
-    hashtable.set('b', 2);
+    h.set('a', 1);
+    h.set('b', 2);
 
-    expect(hashtable.get('a')).toBe(1);
-    expect(hashtable.get('b')).toBe(2);
+    expect(h.get('a')).toBe(1);
+    expect(h.get('b')).toBe(2);
   });
 
   it("should set a value if exists", () => {
-    hashtable.set('a', 1);
-    hashtable.set('a', 2);
-    hashtable.set('a', 3);
-
-    expect(hashtable.get('a')).toBe(3);
+    h.set('a', 1);
+    expect(h.get('a')).toBe(1);
+    h.set('a', 2);
+    expect(h.get('a')).toBe(2);
+    h.set('a', 3);
+    expect(h.get('a')).toBe(3);
   });
 
   it("should handle collisions", () => {
-    hashtable.set('19YN4', 1);
-    hashtable.set('hgLkU', 2);
+    h.set(collidedKey1, 1);
+    h.set(collidedKey2, 2);
 
-    expect(hashtable.get('19YN4')).toBe(1);
-    expect(hashtable.get('hgLkU')).toBe(2);
-    expect(hashtable.collistions).toBe(1);
+    expect(h.get(collidedKey1)).toBe(1);
+    expect(h.get(collidedKey2)).toBe(2);
+    expect(h.collistions).toBe(1);
   });
 
   it("should have correct size", () => {
-    hashtable.set('ab', 1);
-    hashtable.set('ba', 2);
+    h.set(collidedKey1, 1);
+    h.set(collidedKey2, 2);
 
-    expect(hashtable.size).toBe(2);
+    expect(h.size).toBe(2);
 
-    hashtable.set('ba', 3);
-    expect(hashtable.size).toBe(2);
+    h.set(collidedKey2, 3);
+    expect(h.size).toBe(2);
   });
 
   it("should handle many random keys", () => {
@@ -75,44 +74,42 @@ describe("hash table", () => {
     const keys = Array.from({ length: count }, () => randomKey(10));
     const unique = new Set(keys).size;
     for (let i = 0; i < count; i++) {
-      hashtable.set(keys[i], i);
+      h.set(keys[i], i);
     }
 
     for (let i = 0; i < count; i++) {
-      expect(hashtable.get(keys[i])).toBe(i);
+      expect(h.get(keys[i])).toBe(i);
     }
-    expect(hashtable.size).toBe(unique);
-    console.log(hashtable.collistions);
+    expect(h.size).toBe(unique);
+    console.log(h.collistions);
 
     // remove items
     keys.sort((a: string, b: string) => a.localeCompare(b));
     for (let i = 0; i < count; i++) {
-      hashtable.delete(keys[i]);
+      h.delete(keys[i]);
     }
-    expect(hashtable.size).toBe(0);
+    expect(h.size).toBe(0);
 
   });
 
   it("should remove keys", () => {
-    hashtable.set('ab', 1);
-    hashtable.set('ba', 1);
+    h.set(collidedKey1, 1);
+    h.set(collidedKey2, 2);
 
-    expect(hashtable.get('ab')).toBe(1);
-
-    hashtable.delete('ba');
-    expect(hashtable.get('ba')).toBe(undefined);
-    expect(hashtable.size).toBe(1);
+    h.delete(collidedKey2);
+    expect(h.get(collidedKey2)).toBe(undefined);
+    expect(h.size).toBe(1);
   });
 
   it("should be iterable", () => {
-    hashtable.set('ab', 1);
-    hashtable.set('ba', 1);
+    h.set('ab', 1);
+    h.set('ba', 1);
 
-    expect([...hashtable]).toEqual([['ab', 1], ['ba', 1]]);
+    expect([...h]).toEqual([['ab', 1], ['ba', 1]]);
   });
 
   it("can be constructed from iterable", () => {
-    const h = new HashTable([
+    const h = new HashTable<number>([
       ['ab', 1],
       ['ba', 2],
     ])
@@ -121,36 +118,38 @@ describe("hash table", () => {
   });
 
   it("can be constructed from another hash table", () => {
-    hashtable.set('ab', 1);
+    h.set('ab', 1);
 
-    const h = new HashTable(hashtable);
+    const copy = new HashTable(h);
 
-    expect(h.size).toEqual(1);
+    expect(copy.get('ab')).toBe(1);
+    expect(copy.size).toEqual(1);
   });
 
   it("should be clearable", () => {
-    hashtable.set('ab', 1);
-    hashtable.set('ba', 1);
+    h.set('ab', 1);
+    h.set('ba', 1);
 
-    expect(hashtable.size).toEqual(2);
+    expect(h.size).toEqual(2);
 
-    hashtable.clear();
-    expect(hashtable.size).toEqual(0);
+    h.clear();
+    expect(h.get('ab')).toBe(undefined);
+    expect(h.size).toEqual(0);
   });
 
   it("should have 'has' method", () => {
-    hashtable.set('ab', 1);
-    hashtable.set('ba', 1);
+    h.set('ab', 1);
+    h.set('ba', 1);
 
-    expect(hashtable.has('ab')).toBe(true);
-    expect(hashtable.has('absdfsfsf')).toBe(false);
+    expect(h.has('ab')).toBe(true);
+    expect(h.has('absdfsfsf')).toBe(false);
   });
 
   xit("find collided keys", () => {
     for (let i = 0; i < 100; i++) {
       const key1 = randomKey(5);
       const key2 = randomKey(5);
-      if (hashFn(key1, 8) === hashFn(key2, 8)) {
+      if (h.hash(key1) === h.hash(key2)) {
         console.log(key1, key2);
       }
     }
--- src/hashtable.ts
+++ src/hashtable.ts
@@ -3,24 +3,23 @@ interface Bucket<T> {
   value: T;
 }
 
-const initialMaxBuckets = 8;
-
-export const hashFn = (key: string, max: number): number => {
-  let hash = 0;
-  for (let i = 0, len = key.length; i < len; i++) {
-    let chr = key.charCodeAt(i);
-    hash = (hash << 5) - hash + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return Math.abs(hash) % max;
-}
-
 export class HashTable<T = any> implements Iterable<[string, T]> {
-  private maxBuckets = initialMaxBuckets;
+  static readonly initialNBuckets = 8;
+  static hashFn = (key: string): number => {
+    let hash = 0;
+    for (let i = 0, len = key.length; i < len; i++) {
+      let chr = key.charCodeAt(i);
+      hash = (hash << 5) - hash + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return Math.abs(hash);
+  }
+
+  private nBuckets = HashTable.initialNBuckets;
   private buckets: Array<Bucket<T> | undefined> = [];
 
-  private hash(key: string): number {
-    return hashFn(key, this.maxBuckets);
+  public hash(key: string): number {
+    return HashTable.hashFn(key) % this.nBuckets;
   }
 
   public size = 0;
@@ -41,54 +40,67 @@ export class HashTable<T = any> implements Iterable<[string, T]> {
   private init() {
     this.size = 0;
     this.collistions = 0;
-    this.buckets = new Array(this.maxBuckets).fill(undefined);
+    this.buckets = new Array(this.nBuckets).fill(undefined);
   }
 
   public clear() {
-    this.maxBuckets = initialMaxBuckets;
+    this.nBuckets = HashTable.initialNBuckets;
     this.init();
   }
 
-  private setAt(index: number, key: string, value: T) {
-    if (!this.buckets[index % this.maxBuckets]) {
+  private setAt(hash: number, key: string, value: T) {
+    const h = hash % this.nBuckets;
+    if (!this.buckets[h]) {
       this.size++;
     }
-    this.buckets[index % this.maxBuckets] = { key, value };
+    this.buckets[h] = { key, value };
   }
 
-  private collision(index: number, key: string): boolean {
-    const i = index % this.maxBuckets;
-    return !!this.buckets[i] && this.buckets[i]?.key !== key;
+  private findHash(key: string): number | -1 {
+    const hash = this.hash(key);
+    for (let h = hash; h < hash + this.nBuckets; h++) {
+      const bucket = this.buckets[h % this.nBuckets];
+      if (bucket?.key === key) {
+        return h % this.nBuckets;
+      }
+    }
+    return -1;
+  }
+
+  private collision(hash: number, key: string): boolean {
+    const h = hash % this.nBuckets;
+    return !!this.buckets[h] && this.buckets[h]?.key !== key;
   }
 
   set(key: string, value: T) {
-    const index = this.hash(key);
-    if (this.collision(index, key)) {
+    const hash = this.hash(key);
+    if (this.collision(hash, key)) {
       this.collistions++;
 
-      if (this.size > this.maxBuckets * 0.5) {
+      const loadFactor = this.size / this.nBuckets;
+      if (loadFactor > 0.6) {
         this.grow();
         this.set(key, value);
         return;
       }
 
-      for (let i = index + 1; i < index + this.maxBuckets; i++) {
+      for (let h = hash + 1; h < hash + this.nBuckets; h++) {
         // find a suitable bucket
-        if (!this.collision(i, key)) {
-          this.setAt(i, key, value);
+        if (!this.collision(h, key)) {
+          this.setAt(h, key, value);
           return;
         }
       }
       // not found
       throw "Panic";
     } else {
-      this.setAt(index, key, value);
+      this.setAt(hash, key, value);
     }
   }
 
   grow() {
     const copy = [...this.buckets];
-    this.maxBuckets *= 2;
+    this.nBuckets *= 2;
     this.init();
 
     for (let bucket of copy) {
@@ -98,46 +110,35 @@ export class HashTable<T = any> implements Iterable<[string, T]> {
     }
   }
 
-  private findIndex(key: string): number | -1 {
-    const index = this.hash(key);
-    for (let i = index; i < index + this.maxBuckets; i++) {
-      const bucket = this.buckets[i % this.maxBuckets];
-      if (bucket?.key === key) {
-        return i % this.maxBuckets;
-      }
+  get(key: string): T | undefined {
+    const hash = this.findHash(key);
+    if (hash === -1) {
+      return undefined;
     }
-    return -1;
+    return this.buckets[hash]?.value;
   }
 
   has(key: string): boolean {
-    return this.findIndex(key) !== -1;
-  }
-
-  get(key: string): T | undefined {
-    const index = this.findIndex(key);
-    if (index === -1) {
-      return undefined;
-    }
-    return this.buckets[index]?.value;
+    return this.findHash(key) !== -1;
   }
 
   delete(key: string) {
-    const index = this.findIndex(key);
-    if (index === -1) {
+    const hash = this.findHash(key);
+    if (hash === -1) {
       return;
     }
-    this.buckets[index] = undefined;
+    this.buckets[hash] = undefined;
     this.size--;
   }
 
   [Symbol.iterator](): Iterator<[string, T]> {
-    let index = 0;
-    const buckets = this.buckets.filter(item => !!item);
+    let hash = 0;
+    const buckets = this.buckets.filter(bucket => !!bucket);
 
     return {
       next: () => {
-        if (index < buckets.length) {
-          const bucket = buckets[index++];
+        if (hash < buckets.length) {
+          const bucket = buckets[hash++];
           return { value: [bucket!.key, bucket!.value], done: false };
         } else {
           return { value: undefined, done: true };
